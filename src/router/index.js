f3aa0fc12779959609f1e8b1ff43f5e7b4910576
Mingeon Kim <mingeon.dev@gmail.com>
2024-09-03 10:03:38 +0900

    모바일에서 상세 페이지 진입 시 스크롤 위치 오류 수정

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import HomeView from '../views/HomeView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   scrollBehavior(to, from, savedPosition) {
-    return savedPosition ? savedPosition : { top: 0 }
+    return savedPosition ? savedPosition : { top: 0, left: 0 }
   },
   routes: [
     {
